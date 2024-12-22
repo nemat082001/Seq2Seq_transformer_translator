3ff7e5daeebb1087bcfa430059bddd2ad5a134e6
Md Nematullah <mdnematullah.iitkgp@gmail.com>
2024-12-22 14:06:59 +0530

    initial commmit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Seq2Seq_transformer_translator using Pytorch from scratch',
+    author='Md Nematullah',
+    license='MIT',
+)
